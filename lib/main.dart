bdb92da9888f89b576fdefb95d9c914f1f396f52
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-11-25 13:32:01 +0200

    feat: implement responsive layouts with Mobile, Tablet, and Desktop views

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,13 +11,36 @@ class ResponsiveAndAdaptiveUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Center(
-            child: Column(
+        body: LayoutBuilder(
+          builder: (context, constrains) {
+            log('Width: ${constrains.maxWidth}');
+            if (constrains.maxWidth <= 500) {
+              return const MobileLayout();
+            } else if (constrains.maxWidth > 500 &&
+                constrains.maxWidth <= 800) {
+              return const Text('Tablet Layout');
+            }
+            return const Text('Desktop Layout');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CustomMediaQuery extends StatelessWidget {
+  const CustomMediaQuery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: Center(
+        child: Column(
           children: [
             Container(
               color: Colors.red,
@@ -33,7 +58,61 @@ class ResponsiveAndAdaptiveUI extends StatelessWidget {
               width: 100,
             ),
           ],
-        )),
+        ),
+      ),
+    );
+  }
+}
+
+class MobileLayout extends StatelessWidget {
+  const MobileLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ListTileDetails(
+                      number: index + 1,
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                color: Colors.amber,
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text(
+                    '${index + 1}',
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
+
+class ListTileDetails extends StatelessWidget {
+  const ListTileDetails({super.key, required this.number});
+  final int number;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListTile Details'),
+      ),
+      body: Center(
+        child: Text(
+          'You have selected $number',
+          style: const TextStyle(fontSize: 20),
+        ),
       ),
     );
   }
